2f22ae5804d617d617d2f4e8e4617bb71104e039
Callum <callumacourtt@gmail.com>
2023-05-04 19:06:49 +0100

    Added live validation for password

--- src/formValidator.js
+++ src/formValidator.js
@@ -1,42 +1,142 @@
-const zipCodeRegexMap = {
-  USA: /^\d{5}(-\d{4})?$/,
-  Canada: /^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$/,
+const zipCodes = {
+  USA: /^\d{5}(?:[-\s]\d{4})?$/,
+  Canada: /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/,
+  China: /^[1-9]\d{5}$/,
+  India: /^\d{6}$/,
+  Japan: /^\d{3}-?\d{4}$/,
+  UK: /^[A-Z]{1,2}\d[A-Z\d]?\s*\d[A-Z]{2}$/i,
 };
 
-let errorAdded = false;
+class FormValidator {
+  constructor() {
+    this.emailLi = document.querySelector('.emailLi');
+    this.passwordConfirmLi = document.querySelector('.passwordConfirmLi');
+    this.passwordLi = document.querySelector('.passwordLi');
+    this.zipCodeLi = document.querySelector('.zipCodeLi');
 
-export const formValidator = {
-  validateEmail: emailValue => {
-    const regex = /^\S+@\S+\.\S+$/;
-    if (!regex.test(emailValue) && !errorAdded) {
+    // Creating error container divs for each form element
+    this.emailErrorContainer = document.createElement('div');
+    this.emailErrorContainer.classList.add('errorContainer');
+    this.emailLi.appendChild(this.emailErrorContainer);
+
+    this.passwordErrorContainer = document.createElement('div');
+    this.passwordErrorContainer.classList.add('errorContainer');
+    this.passwordLi.appendChild(this.passwordErrorContainer);
+
+    this.passwordConfirmErrorContainer = document.createElement('div');
+    this.passwordConfirmErrorContainer.classList.add('errorContainer');
+    this.passwordConfirmLi.appendChild(this.passwordConfirmErrorContainer);
+
+    this.zipCodeErrorContainer = document.createElement('div');
+    this.zipCodeErrorContainer.classList.add('errorContainer');
+    this.zipCodeLi.appendChild(this.zipCodeErrorContainer);
+  }
+
+  appendError(errorMsg, errorContainer) {
+    if (!errorContainer.errorAdded) {
       const errorMessage = document.createElement('div');
-      errorMessage.innerText =
-        'Please enter a valid email address in the format of name@example.com';
-      errorMessage.style.color = 'red';
-      const emailField = document.querySelector('.emailLi');
-      if (emailField) {
-        emailField.appendChild(errorMessage);
-        errorAdded = true;
-      }
-    } else if (regex.test(emailValue) && errorAdded) {
-      const errorMessage = document.querySelector('.emailLi div');
+      errorMessage.innerText = errorMsg;
+      errorMessage.classList.add('errorMsg');
+      errorContainer.appendChild(errorMessage);
+      errorContainer.errorAdded = true;
+    }
+  }
+
+  removeError(errorContainer) {
+    if (errorContainer.errorAdded) {
+      const errorMessage = errorContainer.querySelector('div');
       errorMessage.parentNode.removeChild(errorMessage);
-      errorAdded = false;
+      errorContainer.errorAdded = false;
     }
-    return regex.test(emailValue);
-  },
+  }
 
-  confirmPassword: (password, passwordConfirm) => {
-    if (password === passwordConfirm) {
-      return true;
+  handleValidationError(isValid, inputValue, errorMsg, errorContainer) {
+    if (!isValid && inputValue.length > 0) {
+      this.appendError(errorMsg, errorContainer);
+    } else {
+      this.removeError(errorContainer);
     }
-    return false;
-  },
+  }
 
-  validateForm: (email, country, zipCode, password, passwordConfirm) => {
-    const invalidEmail = !formValidator.validateEmail(email);
-    const invalidZipCode = !formValidator.validateZipCode(country, zipCode);
-    const nonMatchingPassword = !formValidator.confirmPassword(
+  validateEmail(emailValue) {
+    const regex = /^\S+@\S+\.\S+$/;
+    const isValidEmail = regex.test(emailValue);
+    this.handleValidationError(
+      isValidEmail,
+      emailValue,
+      'Please enter a valid email address in the format name@example.com',
+      this.emailErrorContainer
+    );
+    return isValidEmail;
+  }
+
+  validatePassword(password) {
+    const regex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[a-zA-Z\d!@#$%^&*()_+]{8,}$/i;
+    const isValidPassword = regex.test(password);
+
+    this.hasMinLength = password.length >= 8;
+
+    this.hasLowercase = /[a-z]/.test(password);
+
+    this.hasUppercase = /[A-Z]/.test(password);
+
+    this.hasDigit = /\d/.test(password);
+
+    this.hasSpecialChar = /[!@#$%^&*()_+]/.test(password);
+
+    this.handleValidationError(
+      isValidPassword,
+      password,
+      '',
+      this.passwordErrorContainer
+    );
+    return isValidPassword;
+  }
+
+  confirmPassword(password, passwordConfirm) {
+    const isMatching =
+      password === passwordConfirm && passwordConfirm.length !== 0;
+    this.handleValidationError(
+      isMatching,
+      passwordConfirm,
+      'Non-matching password',
+      this.passwordConfirmErrorContainer
+    );
+    return isMatching;
+  }
+
+  returnZipCodeError(country) {
+    const errorMessages = {
+      USA: 'Please enter a valid ZIP code in the format 12345 or 12345-6789',
+      Canada: 'Please enter a valid postal code in the format A1A 1A1',
+      China: 'Please enter a valid postal code in the format 123456',
+      India: 'Please enter a valid PIN code in the format 123456',
+      Japan: 'Please enter a valid postal code in the format 123-4567',
+      UK: 'Please enter a valid postcode in the format SM6 9RY',
+    };
+    return errorMessages[country];
+  }
+
+  validateZipCode(country, zipValue) {
+    const regex = zipCodes[country];
+    const isValidZipCode = regex.test(zipValue);
+
+    const errorMsg = this.returnZipCodeError(country);
+    this.handleValidationError(
+      isValidZipCode,
+      zipValue,
+      errorMsg,
+      this.zipCodeErrorContainer
+    );
+
+    return isValidZipCode;
+  }
+
+  validateForm(email, country, zipCode, password, passwordConfirm) {
+    const invalidEmail = !this.validateEmail(email);
+    const invalidZipCode = !this.validateZipCode(country, zipCode);
+    const nonMatchingPassword = !this.confirmPassword(
       password,
       passwordConfirm
     );
@@ -46,7 +146,6 @@ export const formValidator = {
       invalidZipCode,
       nonMatchingPassword,
     };
-  },
-};
-
-export default formValidator;
+  }
+}
+export default FormValidator;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,70 @@
-import formValidator from './formValidator';
+import FormValidator from './formValidator';
 import './style.css';
 
 const email = document.querySelector('#email');
-const country = document.querySelector('#country-select');
+const country = document.querySelector('#country');
 const zipCode = document.querySelector('#zipCode');
 const password = document.querySelector('#password');
 const passwordConfirm = document.querySelector('#passwordConfirm');
+const lengthValidator = document.querySelector('.lengthValidator');
+const lowerCaseValidator = document.querySelector('.lowercaseValidator');
+const upperCaseValidator = document.querySelector('.uppercaseValidator');
+const digitValidator = document.querySelector('.digitValidator');
+const specialCharValidator = document.querySelector('.specialCharValidator');
+const form = document.querySelector('.form');
+
+const validator = new FormValidator();
+
+form.addEventListener('submit', event => {
+  const inputs = form.querySelectorAll('input');
+  let isValid = true;
+
+  // Check if any inputs are invalid
+  inputs.forEach(input => {
+    if (!input.checkValidity()) {
+      isValid = false;
+      // Display error message
+      const errorMessage = document.createElement('div');
+      errorMessage.textContent = input.validationMessage;
+      input.parentNode.insertBefore(errorMessage, input.nextSibling);
+    }
+  });
+
+  if (!isValid) {
+    event.preventDefault(); // Prevent form submission
+  }
+});
 
 email.addEventListener('input', () => {
-  formValidator.validateEmail(email.value);
+  validator.validateEmail(email.value);
+});
+
+zipCode.addEventListener('input', () => {
+  validator.validateZipCode(country.value, zipCode.value);
+});
+
+country.addEventListener('change', () => {
+  validator.validateZipCode(country.value, zipCode.value);
+});
+
+function handleValidation(variable, element) {
+  if (variable) {
+    element.classList.add('checked');
+  } else if (element.classList.contains('checked')) {
+    element.classList.remove('checked');
+  }
+}
+
+password.addEventListener('input', () => {
+  validator.validatePassword(password.value);
+  validator.confirmPassword(password.value, passwordConfirm.value);
+  handleValidation(validator.hasMinLength, lengthValidator);
+  handleValidation(validator.hasLowercase, lowerCaseValidator);
+  handleValidation(validator.hasUppercase, upperCaseValidator);
+  handleValidation(validator.hasDigit, digitValidator);
+  handleValidation(validator.hasSpecialChar, specialCharValidator);
+});
+
+passwordConfirm.addEventListener('input', () => {
+  validator.confirmPassword(password.value, passwordConfirm.value);
 });
